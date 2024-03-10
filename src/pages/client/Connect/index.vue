e2ca53d69baec55c5da6b9d9495ea17dd9541fb4
JinxinWu <1762563628@qq.com>
2024-03-10 15:03:39 +0800

    小改动

--- src/pages/client/Connect/index.vue
+++ src/pages/client/Connect/index.vue
@@ -1,79 +1,87 @@
 <template>
-  <el-container>
-    <el-main>
-      <p class="title">中央服务器连接</p>
-      <el-divider></el-divider>
-      <el-row>
-        <el-col :span="8" :offset="8">
-          <div style="margin: 0px"></div>
-          <el-form
-            :model="ruleForm"
-            :rules="rules"
-            ref="ruleForm"
-            label-position="left"
-            label-width="120px"
-            class="ruleForm"
-            :disabled="formDisabled"
-          >
-            <el-form-item label="中央服务器IP" prop="ip">
-              <el-input v-model="ruleForm.ip"></el-input>
-            </el-form-item>
-            <el-form-item label="客户端名称" prop="name">
-              <el-input v-model="ruleForm.name"></el-input>
-            </el-form-item>
-            <el-form-item label="部门" prop="department">
-              <el-select v-model="ruleForm.department" placeholder="请选择部门">
-                <el-option label="一号车间" value="一号车间"></el-option>
-                <el-option label="二号车间" value="二号车间"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')"
-                >请求连接</el-button
-              >
-              <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-          </el-form>
-        </el-col>
-        <el-col :span="8" :offset="8" v-show="messageShow">
-          <el-table
-            v-loading="loading"
-            element-loading-text="正在等待中央服务器的同步消息"
-            :data="tableData"
-            style="width: 100%"
-            :header-cell-style="{ 'text-align': 'center' }"
-            :cell-style="{ 'text-align': 'center' }"
-          >
-            <el-table-column prop="algori" label="算法" width="180">
-            </el-table-column>
-            <el-table-column prop="compress" label="压缩算法" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="xxx"> </el-table-column>
-          </el-table>
-          <div style="margin: 20px"></div>
-          <el-button
-            style="margin-left: 400px"
-            type="primary"
-            @click="agreeMessage()"
-            >方法启用</el-button
-          >
-        </el-col>
-      </el-row>
-    </el-main>
-  </el-container>
+  <div>
+    <p class="title">中央服务器连接</p>
+    <el-divider></el-divider>
+    <el-row>
+      <el-col :span="8" :offset="8">
+        <div style="margin: 0px"></div>
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-position="left"
+          label-width="120px"
+          class="ruleForm"
+          :disabled="formDisabled"
+        >
+          <el-form-item label="中央服务器IP" prop="ip">
+            <el-input v-model="ruleForm.ip"></el-input>
+          </el-form-item>
+          <el-form-item label="客户端名称" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="部门" prop="department">
+            <el-select v-model="ruleForm.department" placeholder="请选择部门" style="width: 100%;">
+              <el-option label="一号车间" value="一号车间"></el-option>
+              <el-option label="二号车间" value="二号车间"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >请求连接</el-button
+            >
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4" :offset="6" v-show="agreeShow">
+        <el-result
+          v-loading="agreeLoading"
+          element-loading-text="中央服务器是否同意连接"
+          :icon="agreeShow ? 'success' : 'error'"
+          :title="agreeShow ? '连接成功' : '拒绝连接'"
+          :subTitle="agreeShow ? '请等待中央服务器同步' : ''"
+        >
+        </el-result>
+      </el-col>
+      <el-col :span="6" :offset="0" v-show="messageShow">
+        <el-table
+          v-loading="loading"
+          element-loading-text="正在等待中央服务器的同步消息"
+          :data="tableData"
+          style="width: 100%"
+          :header-cell-style="{ 'text-align': 'center' }"
+          :cell-style="{ 'text-align': 'center' }"
+        >
+          <el-table-column prop="algori" label="算法" width="180">
+          </el-table-column>
+          <el-table-column prop="compress" label="压缩算法" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="xxx"> </el-table-column>
+        </el-table>
+        <div style="margin: 20px"></div>
+        <el-button
+          style="margin-left: 400px"
+          type="primary"
+          @click="agreeMessage()"
+          >方法启用</el-button
+        >
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-
 import axios from "axios";
 import VueCookies from "vue-cookies";
 
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
-      userId:"",
+      userId: "",
       ruleForm: {
         ip: "",
         name: "",
@@ -97,7 +105,9 @@ export default {
             trigger: "blur",
           },
         ],
-        department: [{ required: true, message: "请选择部门", trigger: "change" }],
+        department: [
+          { required: true, message: "请选择部门", trigger: "change" },
+        ],
       },
       tableData: [
         {
@@ -107,12 +117,14 @@ export default {
         },
       ],
       formDisabled: false,
+      agreeLoading: true,
       loading: true,
       messageShow: false,
+      agreeShow: false,
       token: null,
-      connection:false,
-      socket:null,
-      ip:null,
+      connection: false,
+      socket: null,
+      ip: null,
     };
   },
   created() {
@@ -122,68 +134,68 @@ export default {
     this.socket.onerror = this.onError;
   },
   mounted() {
-    this.init()
+    this.init();
   },
   methods: {
     onOpen(event) {
       // 向服务端发送请求连接信息http://localhost:7000/connect/getConnection
       axios({
         method: "post",
-        url:`http://localhost:7000/connect/getConnection`,
-        headers:{
+        url: `http://localhost:7000/connect/getConnection`,
+        headers: {
           token: this.token,
         },
         data: {
           ip: this.ip,
           userId: this.userId,
           clientName: this.ruleForm.name,
-          department: this.ruleForm.department
+          department: this.ruleForm.department,
         },
-        timeout: 30000
-        }).then((res)=>{
-          this.$message({
-            message: '请求连接信息发送成功',
-            type: 'success'
-          })
-          this.messageShow = true; // 开始转圈等待
+        timeout: 30000,
+      }).then((res) => {
+        this.$message({
+          message: "请求连接信息发送成功",
+          type: "success",
+        });
+        this.messageShow = true; // 开始转圈等待
       });
     },
     onMessage(event) {
-        // 如果是请求处理信息，收到已经同意的信息，转圈等待停止，显示连接成功；如果是拒绝的信息，就关闭websocket
+      // 如果是请求处理信息，收到已经同意的信息，转圈等待停止，显示连接成功；如果是拒绝的信息，就关闭websocket
 
-        this.$notify({
-                  title: "来自中央服务器的连接消息",
-                  message: "连接成功",
-                  duration: 0,
-                  offset: 50,
-                });
-        // 可以禁用表单，可以优化成刷新仍然禁用
-        this.formDisabled = true;
-        
-        // 如果是方法同步信息，则赋值给tableData
-        
-        this.loading = false; // 停止转圈获取到了服务器同步方法信息
-        this.$notify({
-                  title: "来自中央服务器的同步方法消息",
-                  message: "获取同步方法",
-                  duration: 0,
-                  offset: 50,
-                });
-        
-        // this.message = event.data;
-        // console.log('Received message: ' + this.message);
+      this.$notify({
+        title: "来自中央服务器的连接消息",
+        message: "连接成功",
+        duration: 0,
+        offset: 50,
+      });
+      // 可以禁用表单，可以优化成刷新仍然禁用
+      this.formDisabled = true;
+
+      // 如果是方法同步信息，则赋值给tableData
+
+      this.loading = false; // 停止转圈获取到了服务器同步方法信息
+      this.$notify({
+        title: "来自中央服务器的同步方法消息",
+        message: "获取同步方法",
+        duration: 0,
+        offset: 50,
+      });
+
+      // this.message = event.data;
+      // console.log('Received message: ' + this.message);
     },
     onClose(event) {
-        console.log('WebSocket closed');
+      console.log("WebSocket closed");
     },
     onError(event) {
-        console.error('WebSocket error: ' + event);
+      console.error("WebSocket error: " + event);
     },
     init() {
       this.getUserId();
       this.getUserIP((ip) => {
         this.ip = ip;
-        console.log(this.ip)
+        console.log(this.ip);
       });
     },
     getUserId() {
@@ -199,57 +211,68 @@ export default {
           timeout: 30000,
         }).then((res) => {
           this.userId = res.data.userId;
-          console.log(this.userId)
+          console.log(this.userId);
         });
       }
     },
     getUserIP(onNewIP) {
-        let MyPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-        let pc = new MyPeerConnection({
-            iceServers: []
-          });
-        let noop = () => {
-          };
-        let localIPs = {};
-        let ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
-        let isFirstIPFound = false; // 添加一个标志来判断是否已经找到第一个IP
-        let iterateIP = (ip) => {
-          if (!isFirstIPFound) { // 只在第一个IP找到时调用回调函数
-            onNewIP(ip);
-            isFirstIPFound = true;
-          }
-          localIPs[ip] = true;
-        };
-        // let iterateIP = (ip) => {
-        //   if (!localIPs[ip]) onNewIP(ip);
-        //   localIPs[ip] = true;
-        // };
-        pc.createDataChannel('');
-        pc.createOffer().then((sdp) => {
-          sdp.sdp.split('\n').forEach(function (line) {
-            if (line.indexOf('candidate') < 0) return;
+      let MyPeerConnection =
+        window.RTCPeerConnection ||
+        window.mozRTCPeerConnection ||
+        window.webkitRTCPeerConnection;
+      let pc = new MyPeerConnection({
+        iceServers: [],
+      });
+      let noop = () => {};
+      let localIPs = {};
+      let ipRegex =
+        /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+      let isFirstIPFound = false; // 添加一个标志来判断是否已经找到第一个IP
+      let iterateIP = (ip) => {
+        if (!isFirstIPFound) {
+          // 只在第一个IP找到时调用回调函数
+          onNewIP(ip);
+          isFirstIPFound = true;
+        }
+        localIPs[ip] = true;
+      };
+      // let iterateIP = (ip) => {
+      //   if (!localIPs[ip]) onNewIP(ip);
+      //   localIPs[ip] = true;
+      // };
+      pc.createDataChannel("");
+      pc.createOffer()
+        .then((sdp) => {
+          sdp.sdp.split("\n").forEach(function (line) {
+            if (line.indexOf("candidate") < 0) return;
             line.match(ipRegex).forEach(iterateIP);
           });
           pc.setLocalDescription(sdp, noop, noop);
-        }).catch((reason) => {
-        });
-        pc.onicecandidate = (ice) => {
-          if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-          ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-        };
-      },
+        })
+        .catch((reason) => {});
+      pc.onicecandidate = (ice) => {
+        if (
+          !ice ||
+          !ice.candidate ||
+          !ice.candidate.candidate ||
+          !ice.candidate.candidate.match(ipRegex)
+        )
+          return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+      };
+    },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
-        if (valid) { 
-          let url = "http://" + this.ruleForm.ip + "/server/" +this.userId
+        if (valid) {
+          let url = "http://" + this.ruleForm.ip + "/server/" + this.userId;
           // var url="http://localhost:8000/server/"+$("#userId").val();
           // 实现化WebSocket对象，指定要连接的服务器地址与端口  建立连接
           // 等同于socket = new WebSocket("ws://localhost:8000/server");
-          let socketUrl=url.replace("https","ws").replace("http","ws");
+          let socketUrl = url.replace("https", "ws").replace("http", "ws");
           console.log(socketUrl);
-          if(this.socket!=null){
-              this.socket.close();
-              this.socket=null;
+          if (this.socket != null) {
+            this.socket.close();
+            this.socket = null;
           }
           this.socket = new WebSocket(socketUrl);
         } else {
@@ -289,44 +312,6 @@ export default {
 // Loading
 body {
   margin: 0;
-}
-
-// Container
-.el-header {
-  // background-color: #b3c0d1;
-  color: #333;
-  line-height: 60px;
-  padding: 0;
-}
-
-.el-footer {
-  // background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-main {
-  padding: 0;
-  //   height: calc(100vh - 170px); // 设置主体 main 高度
-  // background-color: #e9eef3;
-  color: #333;
-  text-align: left;
-  // line-height: auto;
-}
-
-body > .el-container {
-  // height: 96vh;
-  margin-bottom: 0px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
 }
 
 // Layout
